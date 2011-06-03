3d8e30d4b57ccbdc3aa9bbb43c46c2fba0bb19de
David Gadling <dave@toasterwaffles.com>
2011-06-03 15:47:13 -0700

    Add licensing information

--- mcrender.py
+++ mcrender.py
@@ -1,3 +1,8 @@
+# MCRender by David Gadling is licensed under a
+#   Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+# More details available at http://creativecommons.org/licenses/by-nc-sa/3.0/
+
+
 import subprocess
 import os
 import sys
